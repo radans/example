462b481c47b3f0462579ca4bd1011bde81eec1ce
radans <radan.s@hotmail.com>
2022-06-08 15:05:39 +0200

    new file

---
+++ file.php
@@ -0,0 +1,25 @@
+<?php
+
+public function store(Request $request)
+    {
+        $request->validate([
+            'emri' => 'required',
+            'adresa' => 'required',
+            'qyteti' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required|string|min:4|confirmed',
+            'telefoni' => 'required',
+        ]);
+        $shitesi = User::create([
+            'emri' => $request->emri,
+            'adresa' => $request->adresa,
+            'qyteti' => $request->qyteti,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'telefoni' => $request->telefoni,
+            'niveli' => 'admin',
+        ]);
+        toastr()->success('Shitësi i ri është regjistruar me sukses!');
+        return back();
+
+    }
